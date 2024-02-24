5a42f577957751dc753e0d5de3325d859363386d
LeThamiseur <hpouhe.betanggroup@outlook.com>
2024-02-24 13:41:13 +0100

    Sauvegarde Appr com01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MyFirstApp';
+  title = 'Selfie à wookies';
+  subTitle = 'La super application';
+  logoAffiche = true;
+  lesSelfies : string [] = ['test 01', 'test 02', 'test 03'];
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  apis: {
+    wookoies:{
+      url:'http://urldeprod/....'
+    }
+  }
 };
